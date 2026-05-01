82bd6a43dfc5fef004508d18bd08c41d02fce4c2
JANKI-K <apoorvadixit2204@gmail.com>
2026-05-01 17:04:42 +0530

    fix: add railway domain to allowedOrigins for server actions

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@
 const nextConfig = {
   experimental: {
     serverActions: {
-      allowedOrigins: ["localhost:3000"],
+      allowedOrigins: ["localhost:3000", "taskie-017.up.railway.app"],
     },
   },
 };
